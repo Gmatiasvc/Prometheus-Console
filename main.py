3ee503a49f10da3f91a0b9af8f8e261f9c7499e1
Gmatiasvc <gmatiasvc@hotmail.com>
2022-07-09 16:08:58 -0500

    Added wikipedia support, added multi lang to wiki

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # imports
 import datetime
 import os
+import wikipedia
 
 # variables
 logdatetime = datetime.datetime.now()
@@ -8,6 +9,7 @@ logdatetime = datetime.datetime.now()
 # setup and stuff
 logname = f"{logdatetime.strftime('%Y-%m-%d %H-%M-%S')}.log"
 log = open(f"logs/{logname}", "w")
+
 
 # classes
 class color:
@@ -74,7 +76,10 @@ def main():
                     print(color.reset)
                     break
                 elif exitanswer in ["n", "no"]:
-                    os.remove(logname)
+                    try:
+                        os.remove(f"*/logs/{logname}")
+                    except OSError:
+                        print("Couldn't remove log file: " + logname)
                     print(color.reset)
                     break
                 else:
@@ -86,11 +91,13 @@ def main():
         elif command in ["clear", "cls"]:
             os.system("cls")
             log.write(f"{logdatetime} INFO: Command {command} executed successfully\n")
+            print("Prometheus Command Line Interface [Dev Build 1]")
         
         elif commandToAnalise[0] == "calc":
             
             try:
-                print(f"{commandToAnalise[1]} = {eval(commandToAnalise[1])}")
+                print(f"{commandToAnalise[1]} ",color.fg.green,"=",color.reset,f" {eval(commandToAnalise[1])}")
+                log.write(f"{logdatetime} INFO: Prometheus Command Line Interface sucessfuly evaluated \"{commandToAnalise[1]}\" giving \"{eval(commandToAnalise[1])}\" as ther result, command executeed \"{command}\"\n")
             except ZeroDivisionError:
                 print(color.fg.red,"Error ", color.reset ,"No es posible dividir entre cero, revisa la operacion.")
                 log.write(f"{logdatetime} ERROR: Prometheus Command Line Interface failed to evaluate \"{commandToAnalise[1]}\" due to ZeroDivisionError, command executed \"{command}\"\n")
@@ -104,7 +111,36 @@ def main():
                 print(color.fg.red, "Error ", color.reset, "Un error de punto flotante ha ocurrido durante la evaluacion del problema")
                 log.write(f"{logdatetime} ERROR: Prometheus Command Line Interface failed to evaluate \"{commandToAnalise[1]}\" due to an unknown, command executed \"{command}\"\n")
 
+        elif commandToAnalise[0] in ["wikipedia", "search", "wiki"]:
 
+            if commandToAnalise[1] == "lang":
+            
+                try:
+                    if commandToAnalise[2] == "es":
+                        wikipedia.set_lang("es")
+                        log.write(f"{logdatetime} INFO: Command {command} executed successfully\n")
+                    
+                    elif commandToAnalise[2] == "en":
+                        wikipedia.set_lang("en")
+                        log.write(f"{logdatetime} INFO: Command {command} executed successfully\n")
+
+                    else:
+                        print("There's only 2 languages available: en and es")
+                
+                except Exception:
+                    print("There's only 2 languages available: en and es")
+
+            elif commandToAnalise[1] in ["search", "searchlist", "buscar"]:
+                print(wikipedia.search(commandToAnalise[2]))
+                log.write(f"{logdatetime} INFO: Command {command} executed successfully\n")
+
+            
+            else:
+                try:
+                    print (wikipedia.summary(commandToAnalise[1]))
+                    log.write(f"{logdatetime} INFO: Command {command} executed successfully\n")
+                except Exception:
+                    print(color.fg.red,"Something went wrong, try another language or try being more specific, use \"_\" instead of spaces", color.reset)
 # entry point
 
 if __name__ == '__main__':
